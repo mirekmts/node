72b57302f3d82969089fa1bf586026db186bfa3d
Miroslaw Stasiak <mirekmts@gmail.com>
2017-10-07 21:36:50 +0200

    merge gogle api callback with weather api callback

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,32 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-    // url: 'https://mapogleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    url: 'https://maps.google.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-    json: true
-}, (error, response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log('ERRORMESSAGE', errorMessage)
+    } else {
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+            }
+        });
+    }
 });
