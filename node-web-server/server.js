ac6af2cc954905650f31db6d1cbc6c679c52a70b
Miroslaw Stasiak <mirekmts@gmail.com>
2017-10-08 13:37:58 +0200

    start working with HBS

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello express!</h1>');
-    res.send({
-        name: 'Andrew',
-        likes:
-            ['Bki',
-            'Mom']
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
 
-    })
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        welcomeMessage: 'Hey from hbs',
+        pageTitle: 'Home Page',
+    });
 });
 app.get('/about', (req, res) => {
-    res.send('about page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 })
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
