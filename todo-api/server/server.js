400ed87e0e152dce319913fade5ffd39ce098dbb
Miroslaw Stasiak <mirekmts@gmail.com>
2017-10-10 20:02:25 +0200

    add Patch /todos/:id

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -66,6 +67,32 @@ app.delete('/todos/:id', (req, res) => {
             res.status(404).send();
         }
         res.status(200).send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     })
